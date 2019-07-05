88bcf3755f2320c9ea2859a05a9cb45ed387258e
Ranjitha <27baby1990@gmail.com>
2019-07-05 17:18:52 +0530

    Fourth commit

---
+++ 02Interface/src/com/cogzy/model/Shape.java
@@ -0,0 +1,9 @@
+package com.cogzy.model;
+
+public interface Shape {
+	
+	double PI = 3.14156;
+	
+	void printArea();
+
+}
---
+++ 03Package/src/pack2/D.java
@@ -0,0 +1,14 @@
+package pack2;
+
+import pack1.A;
+
+public class D extends A {
+	public void test() {
+		m3 = 300;
+		m4 = 400;
+
+		A a1 = new A();
+		a1.m4 = 400;
+	}
+
+}
